b9e3d97867861974b193982e9e01b4cc2225091f
Bruno <brunohaas3@hotmail.com>
2026-04-28 09:26:35 -0300

    refactor: extrair domínio cashflow para finance_cashflow.js

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,5 +1,5 @@
 /* Cache version – bump on deploy or use build hash */
-const CACHE_VERSION = "v2-" + "20260426o";
+const CACHE_VERSION = "v2-" + "20260428a";
 const CACHE_NAME = "webdash-" + CACHE_VERSION;
 const PRECACHE = [
   "/",
@@ -9,6 +9,7 @@ const PRECACHE = [
   "/static/app.js",
   "/static/finance.css",
   "/static/finance.js",
+  "/static/finance_cashflow.js",
   "/static/finance_shared.js",
   "/static/finance_flags.js",
   "/static/finance_history.js",
